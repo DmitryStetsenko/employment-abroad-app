8b2de25c72c8ac8b0bf24f697f4a0621a02205da
Stetsenko Dmitry <ebusines2010@gmail.com>
2023-06-13 06:30:05 +0300

    change number on icon

--- src/componets/Hero/HeroTabs.jsx
+++ src/componets/Hero/HeroTabs.jsx
@@ -1,22 +1,22 @@
 import { NavLink } from 'react-router-dom';
 
 const HeroTabs = () => {
-  return (
-    <div className="toggle-tabs">
-        <NavLink to="/" className="btn-tab" data-number="1">
-            <div className="btn-tab__content">
-                <span className="btn-tab__title">Кандидату</span>
-                <span className="btn-tab__subscr">Потреби та пропозиції</span>
-            </div>
-        </NavLink>
-        <NavLink to="/employer" className="btn-tab" data-number="2">
-            <div className="btn-tab__content">
-                <span className="btn-tab__title">Роботодавцю</span>
-                <span className="btn-tab__subscr">Потреби та пропозиції</span>
-            </div>
-        </NavLink>
-    </div>
-  );
+	return (
+		<div className="toggle-tabs">
+			<NavLink to="/" className="btn-tab btn-tab_candidate" data-number="1">
+				<div className="btn-tab__content">
+					<span className="btn-tab__title">Кандидату</span>
+					<span className="btn-tab__subscr">Потреби та пропозиції</span>
+				</div>
+			</NavLink>
+			<NavLink to="/employer" className="btn-tab btn-tab_employer" data-number="2">
+				<div className="btn-tab__content">
+					<span className="btn-tab__title">Роботодавцю</span>
+					<span className="btn-tab__subscr">Потреби та пропозиції</span>
+				</div>
+			</NavLink>
+		</div>
+	);
 };
 
 export default HeroTabs;
