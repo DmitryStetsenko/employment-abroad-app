afcee482cb0b6aee1a0700096974f78aef9dd02b
Stetsenko Dmitry <ebusines2010@gmail.com>
2023-06-13 07:04:19 +0300

    fix seatch result

--- src/componets/forms/SearchForm/SearchResultsItem.jsx
+++ src/componets/forms/SearchForm/SearchResultsItem.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 import { strToSlug } from '../../../store/functions';
 
 const SearchResultsItem = ({ vacancy, closeMethod }) => {
-  const { title, id } = vacancy;
+  const imgPath = '/img/vacancies';
+	const noImg = `${imgPath}/no-img.jpg`;
+  const { title, id, thumbnails } = vacancy;
 
   return (
     <Link onClick={ closeMethod } to={`/vacancy/${strToSlug(title)}`} state={ id } className="vacancy-search-item">
       <div className="vacancy-search-item__thumbnail">
-        <img src="/img/vacancy-thumb.jpg" alt="" />
+      <img src={ thumbnails ? `${imgPath}/${thumbnails}` : noImg } alt={ title } />
       </div>
       <p className="vacancy-search-item__title">{ title }</p>
     </Link>
