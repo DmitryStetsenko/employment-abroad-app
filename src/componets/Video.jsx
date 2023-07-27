19d550ef691997875f145c8828a52e5ff203de86
Dmitry Stetsenko <ebusines2010@gmail.com>
2023-07-27 11:25:32 +0300

    start dev on video block

---
+++ src/componets/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Video = () => {
+  return (
+    <div className="video">
+      <i class="fa-brands fa-youtube"></i>
+    </div>
+  );
+};
+
+export default Video;
